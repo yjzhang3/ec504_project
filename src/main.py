d623a198de6550cdd5cd02d62110d850adfe9762
Ronnakorn Rattanakornphan <ronrat@bu.edu>
2021-05-01 08:03:52 +0700

    add largest common component scoring + result json

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import sys
 import time
+import json
 
 from GraphAdjList import *
 from matrix import *
 from Visual import print_network
-from scoring_func import edgeCorr
+from scoring_func import score
 ### default file location
 file1_location = "./data/set3_a.txt"
 file2_location = "./data/set3_b.txt"
@@ -28,12 +29,8 @@ if len(sys.argv) > 1:
         print('command ', sys.argv[1], 'is not recogized')
         exit(1)
 
-
-
 print("loading graph1 from: ", file1_location)
 print("loading graph2 from: ", file2_location)
-
-
 
 # making grpah1 and graph2, reading information from the provided file
 graph1, graph2, map1, map2 = make_graph(file1_location, file2_location)
@@ -47,28 +44,44 @@ a = 1
 b = 0 # we don't use biological data for this testing
 lamb_da = 0.5
 
+
 ### called the alignment function
 start_time = time.time()
 result = align(graph1, graph2, a, b, lamb_da)
-print("Align time --- %s seconds ---" % (time.time() - start_time))
+elapse_time = time.time() - start_time
+print("Align time --- %s seconds ---" % (elapse_time))
 ### result is a dictionary, {id_in_graph1, id_in_graph2}, basically a bunch of matching between i,j
-
-### TO DO:
-### calculate the performance score, using alignment
-
 
 inv_map1 = {v: k for k, v in map1.items()}
 inv_map2 = {v: k for k, v in map2.items()}
 
-### remapped the id to the protein name
-# mapped_result = [(inv_map1[i], inv_map2[j]) for (i,j) in result]
+### remapped the id to the 'protein name'
 mapped_result = [(inv_map1[i], inv_map2[j]) for (i,j) in result.items()]
 print('Result:')
 print(mapped_result)
 
-EC_score = edgeCorr(result, graph1, graph2)
+EC_score, largest_cc = score(result, graph1, graph2)
+largest_cc = list(largest_cc)
+largest_cc = [inv_map1[item] for item in largest_cc]
+
 print("edge correctness = ", EC_score)
+print("largest_cc = ", largest_cc)
+print("lengeth largest_cc = ", len(largest_cc))
+
+### ouputing the result
+data = {}
+data['result'] = mapped_result
+data['edge correctness'] = EC_score
+data['LCCS'] = largest_cc
+data['LCCS_len'] = len(largest_cc)
+### save result into .json file
+### change 'data' to the filename we want
+with open('result/data.json', 'w') as fp:
+    json.dump(data, fp)
+
+
+### print the 2 compared graph, with color coded
 print_network(file1_location, file2_location, mapped_result)
 
-### save result into .json file
-### which the frontend will read later
+
+
--- src/scoring_func.py
+++ src/scoring_func.py
@@ -1,35 +1,39 @@
 #https://angom.myweb.cs.uwindsor.ca/teaching/cs592/592-ST-NSB-NetAlignment.pdf
 
-# ran  = [(i,j) for i in range(3) for j in range(3)]
-# graph_len = 70
-
-# def edgeCorr(al,graph_len):
-#     match= 0
-#     for pair in range(len(al)):
-#         if al[pair][0] = al[pair][1]:
-#             match += 1
-
-#     return match/graph_len
-
-# ec = edgeCorr(ran,graph_len)
-# print(ec)
+import networkx as nx
 
 ### al = result pair mapping 
-def edgeCorr(al, graph1, graph2):
+def score(al, graph1, graph2):
     match = 0
     size = len(graph1['edges'])
+    ### use this to compute LCCS (Largest common component)
+    G = nx.Graph() 
+
+
     ### loop over all edges in graph1
     for edge in graph1['edges']:
+        
         ### map edge (i1, i2) to (j1, j2)
         i1 = edge[0]
         i2 = edge[1]
 
         j1 = al[i1]
         j2 = al[i2]
+
+
         for neighbor in graph2[j1]:
             ### if edge (j1, j2) exist in graph2, score + 1
             if neighbor[0] == j2:
                 match += 1
+                G.add_edge(i1, i2)
+                
                 break
-    ### score = total correct edge match / total number of edges
-    return match/size
+    ### edge correctness score = total correct edge match / total number of edges
+    EC_score = match/size
+    largest_cc = max(nx.connected_components(G), key=len)
+    return EC_score, largest_cc
+
+# LCCS
+# get all 'correct' edge map
+# make a graph out of it
+# find largest connected component
